1422b5b81fab6e0a053e6226d0c4fe46854bbe61
ckuetbach <879891+ckuetbach@users.noreply.github.com>
2019-12-04 16:08:48 +0100

    - workarround for: https://github.com/NuGet/Home/issues/4393

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -19,12 +19,12 @@ if [[ ${TRAVIS_PULL_REQUEST} == 'false' ]]; then
         echo "pack release"
         dotnet pack -c Release /p:Version=${VERSION}.${TRAVIS_BUILD_NUMBER} /p:InformationalVersion="$VERSION+$TRAVIS_BRANCH/$TRAVIS_COMMIT"
         echo "push"
-        dotnet nuget push ./**/bin/Release/*.$VERSION.$TRAVIS_BUILD_NUMBER.nupkg -s https://api.nuget.org/v3/index.json -k $NUGET_API_KEY
+        find . -name '*.$VERSION.$TRAVIS_BUILD_NUMBER.nupkg' | xargs -i dotnet nuget push {} -s https://api.nuget.org/v3/index.json -k $NUGET_API_KEY
     else
         echo "pack prerelease"
         dotnet pack -c Release /p:Version=${VERSION}.${TRAVIS_BUILD_NUMBER}-prerelease /p:InformationalVersion="$VERSION+$TRAVIS_BRANCH/$TRAVIS_COMMIT"
         echo "push"
-        dotnet nuget push ./**/bin/Release/*.$VERSION.$TRAVIS_BUILD_NUMBER-prerelease.nupkg -s https://api.nuget.org/v3/index.json -k $NUGET_API_KEY
+        find . -name '*.$VERSION.$TRAVIS_BUILD_NUMBER-prerelease.nupkg' | xargs -i dotnet nuget push {} -s https://api.nuget.org/v3/index.json -k $NUGET_API_KEY
     fi
 fi
 echo "return code: "
