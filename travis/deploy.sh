ce7647d6594d99a3ce1614acf32e916462fc9cfa
ckuetbach <879891+ckuetbach@users.noreply.github.com>
2019-12-04 13:44:02 +0100

    - fixed wrong name for environment variable

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -8,12 +8,12 @@ dotnet clean
 
 if [ ! -z "$TRAVIS_TAG" ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     echo "pack release"
-    dotnet pack -c Release /p:Version=$VERSION.$TRAVIS_BUILD /p:InformationalVersion="$VERSION+$TRAVIS_BRANCH/$TRAVIS_COMMIT"
+    dotnet pack -c Release /p:Version=$VERSION.$TRAVIS_BUILD_NUMBER /p:InformationalVersion="$VERSION+$TRAVIS_BRANCH/$TRAVIS_COMMIT"
     #echo "push"
-    #dotnet nuget push ./dvelop.NugetPushTest/bin/Release/dvelop.NugetPushTest.$VERSION.$TRAVIS_BUILD.nupkg -s https://api.nuget.org/v3/index.json -k $NUGET_API_KEY
+    #dotnet nuget push ./dvelop.NugetPushTest/bin/Release/dvelop.NugetPushTest.$VERSION.$TRAVIS_BUILD_NUMBER.nupkg -s https://api.nuget.org/v3/index.json -k $NUGET_API_KEY
 else
     echo "pack prerelease"
-    dotnet pack -c Release /p:Version=$VERSION.$TRAVIS_BUILD-prerelease /p:InformationalVersion="$VERSION+$TRAVIS_BRANCH/$TRAVIS_COMMIT"
+    dotnet pack -c Release /p:Version=$VERSION.$TRAVIS_BUILD_NUMBER-prerelease /p:InformationalVersion="$VERSION+$TRAVIS_BRANCH/$TRAVIS_COMMIT"
     #echo "push"
-    #dotnet nuget push ./**/bin/Release/*.$VERSION.$TRAVIS_BUILD-prerelease.nupkg -s https://api.nuget.org/v3/index.json -k $NUGET_API_KEY
+    #dotnet nuget push ./**/bin/Release/*.$VERSION.$TRAVIS_BUILD_NUMBER-prerelease.nupkg -s https://api.nuget.org/v3/index.json -k $NUGET_API_KEY
 fi
