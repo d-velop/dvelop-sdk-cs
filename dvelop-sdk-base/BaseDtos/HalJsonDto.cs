7fb473ccbc4919e1524e3d9017bbefa19f04f91b
Terschluse Sven <sven.terschluse@d-velop.de>
2020-01-07 13:57:43 +0100

    Added _embedded to HalJsonDto

--- dvelop-sdk-base/BaseDtos/HalJsonDto.cs
+++ dvelop-sdk-base/BaseDtos/HalJsonDto.cs
@@ -7,8 +7,12 @@ namespace Dvelop.Sdk.Base.Dto
         public HalJsonDto()
         {
             _links = new Dictionary<string, RelationDataDto>();
+            _embedded = new Dictionary<string, object>();
         }
         // ReSharper disable once InconsistentNaming
         public Dictionary<string, RelationDataDto> _links { get; set; }
+        
+        // ReSharper disable once InconsistentNaming
+        public Dictionary<string, object> _embedded { get; set; }
     }
 }
