aa86076dfb8b83e555a5bc095f732ec7393a6f1b
Sven Terschluse <47656372+sterschl@users.noreply.github.com>
2020-02-18 20:09:20 +0100

    Added _embedded to HalJsonDto (#2)
    * Added _embedded to HalJsonDto
    
    * Do not serialize properties when they are empty
    
    * Do not serialize templated in relationDataDto if false

--- dvelop-sdk-base/BaseDtos/HalJsonDto.cs
+++ dvelop-sdk-base/BaseDtos/HalJsonDto.cs
@@ -7,8 +7,21 @@ namespace Dvelop.Sdk.Base.Dto
         public HalJsonDto()
         {
             _links = new Dictionary<string, RelationDataDto>();
+            _embedded = new Dictionary<string, object>();
         }
         // ReSharper disable once InconsistentNaming
         public Dictionary<string, RelationDataDto> _links { get; set; }
+        
+        // ReSharper disable once InconsistentNaming
+        public Dictionary<string, object> _embedded { get; set; }
+        
+        public bool ShouldSerialize_links()
+        {
+            return _links?.Count > 0;
+        }
+        public bool ShouldSerialize_embedded()
+        {
+            return _embedded?.Count > 0;
+        }
     }
 }
