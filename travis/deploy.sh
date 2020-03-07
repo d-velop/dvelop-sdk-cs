41cb6fa3ee1401d06d5192cacdaf07e7737626dd
ckuetbach <879891+ckuetbach@users.noreply.github.com>
2020-03-07 21:33:26 +0100

    Removed version.txt and added VersionPrefix to reduce complexity

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 # http://redsymbol.net/articles/unofficial-bash-strict-mode/
 
-export VERSION=$(<./version.txt)
+export VERSION=$(grep -oPm1 "(?<=<VersionPrefix>)[^<]+" Directory.Build.props)
 
 echo ${TRAVIS_BUILD_NUMBER}
 
@@ -27,9 +27,11 @@ echo "build"
     else
         echo "pack prerelease"
         dotnet build -c Debug
-		dotnet pack -c Debug /p:Version=${VERSION}.${TRAVIS_BUILD_NUMBER}-prerelease /p:InformationalVersion="$VERSION+$TRAVIS_BRANCH/$TRAVIS_COMMIT"
-        echo "push"
-        find . -name "*.$VERSION.$TRAVIS_BUILD_NUMBER-prerelease.nupkg" | xargs -i dotnet nuget push {} -s https://api.nuget.org/v3/index.json -k ${NUGET_API_KEY}
+		if [[ ${TRAVIS_BRANCH} == 'master' ]]; then 
+            dotnet pack -c Debug /p:Version=${VERSION}.${TRAVIS_BUILD_NUMBER}-prerelease /p:InformationalVersion="$VERSION+$TRAVIS_BRANCH/$TRAVIS_COMMIT"
+            echo "push"
+            find . -name "*.$VERSION.$TRAVIS_BUILD_NUMBER-prerelease.nupkg" | xargs -i dotnet nuget push {} -s https://api.nuget.org/v3/index.json -k ${NUGET_API_KEY}
+        fi
     fi
 fi
 echo "return code: $?"
