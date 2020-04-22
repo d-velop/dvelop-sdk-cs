9b5682d951374975aeef38d2b65c1b0b840c6099
ckuetbach <879891+ckuetbach@users.noreply.github.com>
2020-04-22 22:08:20 +0200

    Added some Tests

---
+++ dvelop-sdk-identityprovider/IdentityProviderMiddleware.UnitTest/IdentityProviderExtensionsTest.cs
@@ -0,0 +1,28 @@
+using Dvelop.Sdk.IdentityProvider.Middleware;
+using Microsoft.AspNetCore.Http;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Dvelop.Sdk.IdentityProviderMiddleware.UnitTest
+{
+    [TestClass]
+    public class IdentityProviderExtensionsTest
+    {
+        [DataTestMethod]
+        [DataRow( null, null, null )]
+        [DataRow( "123", null, null )]
+        [DataRow( "123&abc", null, "123&abc" )]
+        public void TestGetAuthSessionIdFromBearer(string actualBearer, string actualCookie, string expected)
+        {
+            var context = new DefaultHttpContext();
+            context.Request.Method = "GET";
+            context.Request.Path = "/some/uri";
+            if (actualBearer != null)
+            {
+                context.Request.Headers["Authorization"]=$"Bearer {actualBearer}";
+            }
+
+            var c = context.GetAuthSessionId();
+            Assert.AreEqual(expected, c);
+        }
+    }
+}
