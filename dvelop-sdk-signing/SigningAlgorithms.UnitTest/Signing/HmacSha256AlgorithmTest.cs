ea8041e66eb500a65889e6954fdf7db35e3b130e
ckuetbach <879891+ckuetbach@users.noreply.github.com>
2020-05-04 08:50:25 +0200

    - Verified tests - Added some Tests

--- dvelop-sdk-signing/SigningAlgorithms.UnitTest/Signing/HmacSha256AlgorithmTest.cs
+++ dvelop-sdk-signing/SigningAlgorithms.UnitTest/Signing/HmacSha256AlgorithmTest.cs
@@ -14,6 +14,8 @@ namespace Dvelop.Sdk.SigningAlgorithms.UnitTest.Signing
         [DataTestMethod]
         [DataRow("","e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855")]
         [DataRow("Input","36ecb4f8669133ce744c21982ba4abe2ecd7086e1dc2226ccd6f266f3a5005f8")]
+        [DataRow("{\"type\":\"subscribe\",\"tenantId\":\"id\",\"baseUri\":\"https://someone.d-velop.cloud\"}\n", "c2a6fefc93b809eeaf2f069504fe8e02b0f3341b3c5e488e6a402ca45301415c")]
+        [DataRow("POST\n/myapp/dvelop-cloud-lifecycle-event\n\nx-dv-signature-algorithm:DV1-HMAC-SHA256\nx-dv-signature-headers:x-dv-signature-algorithm,x-dv-signature-headers,x-dv-signature-timestamp\nx-dv-signature-timestamp:2019-08-09T08:49:42Z\n\nc2a6fefc93b809eeaf2f069504fe8e02b0f3341b3c5e488e6a402ca45301415c","fcecaac3dae4d40d6f2a065678f59f4794dfbe8497fe9ca825f737299887ebf4")]
         public void TestSha256(string input, string expected)
         {
             Assert.AreEqual( expected, HmacSha256Algorithm.Sha256( input ));
@@ -23,10 +25,17 @@ namespace Dvelop.Sdk.SigningAlgorithms.UnitTest.Signing
         [DataTestMethod]
         [DataRow("","14cec2793d3c3b01e0d5730d79f187141f9a8ce9e9fa1815a73ffb9df6d61bbe")]
         [DataRow("Input","15c8b4a5aedee67aa3eb383f45ed8dde8713a7e2c94e62ad37fc4aaadda8cd3a")]
-        [DataRow("fbfcc87ba2b7ae35506a780388bef9eccb25d734e4a90ad75e8da8002d443128", "fbfcc87ba2b7ae35506a780388bef9eccb25d734e4a90ad75e8da8002d443128")]
+        [DataRow("fbfcc87ba2b7ae35506a780388bef9eccb25d734e4a90ad75e8da8002d443128", "cc2bfbac52f30ddee41e4475963f8136c60cab678941560538b1281ad2722aac")]
         public void HmacSha256(string input, string expected)
         {
             Assert.AreEqual( expected, HmacSha256Algorithm.HmacSha256( Convert.FromBase64String(Key) , input ));
+        }
+        
+        [DataTestMethod]
+        [DataRow("POST\n/myapp/dvelop-cloud-lifecycle-event\n\nx-dv-signature-algorithm:DV1-HMAC-SHA256\nx-dv-signature-headers:x-dv-signature-algorithm,x-dv-signature-headers,x-dv-signature-timestamp\nx-dv-signature-timestamp:2019-08-09T08:49:42Z\n\nc2a6fefc93b809eeaf2f069504fe8e02b0f3341b3c5e488e6a402ca45301415c", "02783453441665bf27aa465cbbac9b98507ae94c54b6be2b1882fe9a05ec104c")]
+        public void HmacSha256WithBodyHash(string input, string expected)
+        {
+            Assert.AreEqual( expected, HmacSha256Algorithm.HmacSha256( Convert.FromBase64String(Key) , HmacSha256Algorithm.Sha256(input) ));
         }
     }
 }
--- dvelop-sdk-webapi/WebApiExtensions.UnitTest/Extensions/Dv1HmacSha256RequestSigningExtensionTest.cs
+++ dvelop-sdk-webapi/WebApiExtensions.UnitTest/Extensions/Dv1HmacSha256RequestSigningExtensionTest.cs
@@ -14,31 +14,31 @@ namespace Dvelop.Sdk.WebApiExtensions.UnitTest.Extensions
     [ExcludeFromCodeCoverage]
     public class Dv1HmacSha256RequestSigningExtensionTest
     {
-        private const string ExampleBody = "{\"type\":\"subscribe\",\"tenantId\":\"id\",\"baseUri\":\"https://someone.d-velop.cloud\"}\n";
-        
         [TestMethod]
-        public async Task TestCalculateDv1HmacSha256Signature()
+        public async Task TestCalculateDv1HmacSha256SignatureFromExample()
         {
             var x = new DefaultHttpRequest(new DefaultHttpContext())
             {
                 Method = "POST",
                 Path = "/myapp/dvelop-cloud-lifecycle-event",
                 ContentType = "application/json",
-                Body = new MemoryStream(Encoding.UTF8.GetBytes(ExampleBody)),
+                Body = new MemoryStream(Encoding.UTF8.GetBytes("{\"type\":\"subscribe\",\"tenantId\":\"id\",\"baseUri\":\"https://someone.d-velop.cloud\"}\n")),
                 Headers =
                 {
                     {"x-dv-signature-timestamp","2019-08-09T08:49:42Z"},
                     {"x-dv-signature-algorithm", "DV1-HMAC-SHA256"},
                     {"x-dv-signature-headers", "x-dv-signature-algorithm,x-dv-signature-headers,x-dv-signature-timestamp"},
-                    {"Authorization","Bearer 58b07086ef6d987016d35c8ca2b0c1e48ed1aa8ffe31819402ad8f06c7bd4486"}
+                    {"Authorization","Bearer 02783453441665bf27aa465cbbac9b98507ae94c54b6be2b1882fe9a05ec104c"}
                 }
             };
             var features = x.HttpContext.Features.Get<IHttpRequestFeature>();
-            features.RawTarget = "https://acme-apptemplate.service.d-velop.cloud/prod/acme-apptemplatecs/dvelop-cloud-lifecycle-event";
+            features.RawTarget = "https://acme-apptemplate.service.d-velop.cloud/myapp/dvelop-cloud-lifecycle-event";
             var calculated = await x.CalculateDv1HmacSha256Signature("Rg9iJXX0Jkun9u4Rp6no8HTNEdHlfX9aZYbFJ9b6YdQ=");
-            Assert.AreEqual( "58b07086ef6d987016d35c8ca2b0c1e48ed1aa8ffe31819402ad8f06c7bd4486", calculated );
+            Assert.AreEqual( "02783453441665bf27aa465cbbac9b98507ae94c54b6be2b1882fe9a05ec104c", calculated );
         }
 
+        
+        
         [TestMethod]
         public async Task TestCalculateDv1HmacSha256Signature1()
         {
@@ -53,13 +53,37 @@ namespace Dvelop.Sdk.WebApiExtensions.UnitTest.Extensions
                     {"x-dv-signature-timestamp","2020-04-30T08:16:40Z"},
                     {"x-dv-signature-algorithm", "DV1-HMAC-SHA256"},
                     {"x-dv-signature-headers", "x-dv-signature-algorithm,x-dv-signature-headers,x-dv-signature-timestamp"},
-                    {"Authorization","Bearer 93e7a60e2209b7c2b2f73ef082a77aacedbfed1aa14077312293d08aa1940b08"}
+                    {"Authorization","Bearer cc2bfbac52f30ddee41e4475963f8136c60cab678941560538b1281ad2722aac"}
                 }
             };
             var features = x.HttpContext.Features.Get<IHttpRequestFeature>();
             features.RawTarget = "https://acme-apptemplate.service.d-velop.cloud/prod/acme-apptemplatecs/dvelop-cloud-lifecycle-event";
             var calculated = await x.CalculateDv1HmacSha256Signature("Rg9iJXX0Jkun9u4Rp6no8HTNEdHlfX9aZYbFJ9b6YdQ=");
-            Assert.AreEqual( "93e7a60e2209b7c2b2f73ef082a77aacedbfed1aa14077312293d08aa1940b08", calculated);
+            Assert.AreEqual( "cc2bfbac52f30ddee41e4475963f8136c60cab678941560538b1281ad2722aac", calculated);
+        }
+        
+        
+        [TestMethod]
+        public async Task TestCalculateDv1HmacSha256Signature2()
+        {
+            var x = new DefaultHttpRequest(new DefaultHttpContext())
+            {
+                Method = "POST",
+                Path = "/prod/acme-apptemplatecs/dvelop-cloud-lifecycle-event",
+                ContentType = "application/json",
+                Body = new MemoryStream(Encoding.UTF8.GetBytes("{\"type\":\"subscribe\",\"tenantId\":\"id\",\"baseUri\":\"https://someone.d-velop.cloud\"}\n")),
+                Headers =
+                {
+                    {"x-dv-SIGNATURE-timestamp","2019-08-09T08:49:42Z"},
+                    {"x-dv-signature-algorithm", "DV1-HMAC-SHA256"},
+                    {"x-dv-signature-headers", "x-dv-signature-algorithm,x-dv-signature-headers,x-dv-signature-timestamp"},
+                    {"Authorization","Bearer 58b07086ef6d987016d35c8ca2b0c1e48ed1aa8ffe31819402ad8f06c7bd4486"}
+                }
+            };
+            var features = x.HttpContext.Features.Get<IHttpRequestFeature>();
+            features.RawTarget = "https://acme-apptemplate.service.d-velop.cloud/prod/acme-apptemplatecs/dvelop-cloud-lifecycle-event";
+            var calculated = await x.CalculateDv1HmacSha256Signature("Rg9iJXX0Jkun9u4Rp6no8HTNEdHlfX9aZYbFJ9b6YdQ=");
+            Assert.AreEqual( "58b07086ef6d987016d35c8ca2b0c1e48ed1aa8ffe31819402ad8f06c7bd4486", calculated);
         }
         
     }
