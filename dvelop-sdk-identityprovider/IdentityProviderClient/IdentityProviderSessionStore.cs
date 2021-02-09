401f7f2c5f48505cb49d827301065342901af6c3
ckuetbach <879891+ckuetbach@users.noreply.github.com>
2021-02-09 10:28:59 +0100

    Added tests for Caching

--- dvelop-sdk-identityprovider/IdentityProviderClient/IdentityProviderSessionStore.cs
+++ dvelop-sdk-identityprovider/IdentityProviderClient/IdentityProviderSessionStore.cs
@@ -1,29 +1,89 @@
 ﻿using System;
+using System.Collections.Concurrent;
+
 using System.Security.Claims;
-using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Internal;
 
 namespace Dvelop.Sdk.IdentityProvider.Client
 {
     public class IdentityProviderSessionStore
     {
-        private readonly IMemoryCache _memoryCache = new MemoryCache(new MemoryCacheOptions());
+        private readonly ISystemClock _clock;
+        private readonly int _cleanupThreshold;
+
+        private readonly ConcurrentDictionary<string, IdentityProviderSessionItem> _sessionCache =
+            new ConcurrentDictionary<string, IdentityProviderSessionItem>();
+
+        private int _cleanupCounter;
+        private readonly object _cleanupLock=new object();
+
+        public IdentityProviderSessionStore(ISystemClock clock, int cleanupThreshold)
+        {
+            _clock = clock;
+            _cleanupThreshold = cleanupThreshold;
+        }
+
+        public IdentityProviderSessionStore():this(new SystemClock(), 20)
+        {
+            
+        }
         
+
         public ClaimsPrincipal GetPrincipal(string cookie)
         {
-            if (!_memoryCache.TryGetValue(cookie, out IdentityProviderSessionItem sessionItem)) return null;
-            return sessionItem.Expire > DateTimeOffset.Now ? sessionItem.Principal : null;
+            CleanUp();
+            var id = IdFromCookie(cookie);
+            if (!_sessionCache.TryGetValue(id, out var sessionItem)) return null;
+            if (sessionItem.Expire.CompareTo(_clock.UtcNow) < 0)
+            {
+                _sessionCache.TryRemove(id,out _);
+                return null;
+            }
+            return sessionItem.Cookie.Equals(cookie) ? sessionItem.Principal : null;
         }
 
         public void SetPrincipal(string cookie, DateTimeOffset expire, ClaimsPrincipal principal)
         {
+            CleanUp();
+            var id = IdFromCookie(cookie);
             var sessionItem = new IdentityProviderSessionItem
             {
                 Cookie = cookie,
                 Principal = principal,
                 Expire = expire
             };
-            _memoryCache.Set(cookie, sessionItem, new MemoryCacheEntryOptions { AbsoluteExpiration =  expire});
+            _sessionCache[id] = sessionItem;
         }
 
+        private void CleanUp()
+        {
+            _cleanupCounter++;
+            if (_cleanupCounter < _cleanupThreshold) return;
+            lock (_cleanupLock)
+            {
+                if (_cleanupCounter < _cleanupThreshold) return;
+                _cleanupCounter = 0;
+                try
+                {
+                    foreach (var key in _sessionCache.Keys)
+                    {
+                        if (!_sessionCache.TryGetValue(key, out var sessionItem)) continue;
+                        if (sessionItem.Expire.CompareTo(_clock.UtcNow) < 0)
+                        {
+                            _sessionCache.TryRemove(key, out _);
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+                    //ignorieren
+                }
+            }
+        }
+
+        private static string IdFromCookie(string cookie)
+        {
+            return cookie;
+        }
     }
 }
--- dvelop-sdk-identityprovider/IdentityProviderMiddleware.UnitTest/IdentityProviderSessionStoreTest.cs
+++ dvelop-sdk-identityprovider/IdentityProviderMiddleware.UnitTest/IdentityProviderSessionStoreTest.cs
@@ -1,26 +1,30 @@
 using System;
 using System.Security.Claims;
 using Dvelop.Sdk.IdentityProvider.Client;
+using Microsoft.Extensions.Internal;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 
 namespace Dvelop.Sdk.IdentityProviderMiddleware.UnitTest
 {
     [TestClass]
     public class IdentityProviderSessionStoreTest
     {
-        private IdentityProviderSessionStore unit;
+        private IdentityProviderSessionStore _unit;
+        private Mock<ISystemClock> _clock;
 
         [TestInitialize]
         public void Setup()
         {
-            unit = new IdentityProviderSessionStore();
+            _clock = new Mock<ISystemClock> {CallBase = true};
+            _unit = new IdentityProviderSessionStore(_clock.Object, 2);
         }
         
         
         [TestMethod]
         public void GetUnknownCookieReturnsNull()
         {
-            Assert.IsNull(unit.GetPrincipal("a&1"));
+            Assert.IsNull(_unit.GetPrincipal("a&1"));
         }
         
         [TestMethod]
@@ -28,8 +32,8 @@ namespace Dvelop.Sdk.IdentityProviderMiddleware.UnitTest
         {
             var cookie = "a&1";
             var claimsPrincipal = new ClaimsPrincipal();
-            unit.SetPrincipal(cookie, DateTime.Now.AddHours(1), claimsPrincipal );
-            Assert.AreEqual(claimsPrincipal, unit.GetPrincipal(cookie));
+            _unit.SetPrincipal(cookie, DateTime.Now.AddHours(1), claimsPrincipal );
+            Assert.AreEqual(claimsPrincipal, _unit.GetPrincipal(cookie));
         }
         
         
@@ -39,8 +43,8 @@ namespace Dvelop.Sdk.IdentityProviderMiddleware.UnitTest
             var user1 = "a&1";
             var user2 = "b&1";
             var claimsPrincipal = new ClaimsPrincipal();
-            unit.SetPrincipal(user1, DateTime.Now.AddHours(1), claimsPrincipal );
-            Assert.IsNull(unit.GetPrincipal(user2 ));
+            _unit.SetPrincipal(user1, DateTime.Now.AddHours(1), claimsPrincipal );
+            Assert.IsNull(_unit.GetPrincipal(user2 ));
         }
         
                 
@@ -50,9 +54,57 @@ namespace Dvelop.Sdk.IdentityProviderMiddleware.UnitTest
             var user1 = "a&1";
             var user2 = "a&2";
             var claimsPrincipal = new ClaimsPrincipal();
-            unit.SetPrincipal(user1, DateTime.Now.AddHours(1), claimsPrincipal );
-            Assert.AreEqual(claimsPrincipal, unit.GetPrincipal(user1));
-            Assert.IsNull(unit.GetPrincipal(user2));
+            _unit.SetPrincipal(user1, DateTime.Now.AddHours(1), claimsPrincipal );
+            Assert.AreEqual(claimsPrincipal, _unit.GetPrincipal(user1));
+            Assert.IsNull(_unit.GetPrincipal(user2));
+        }
+
+        [TestMethod]
+        public void GetExpiredItemShouldReturnNull()
+        {
+            var now = DateTimeOffset.UtcNow;
+            _clock.SetupSequence(c => c.UtcNow)
+                .Returns(now)                   // SET 
+                .Returns(now.AddMinutes(59))    // GET 1
+                .Returns(now.AddMinutes(61));   // GET 2
+            
+            const string user1 = "a&1";
+            var claimsPrincipal = new ClaimsPrincipal();
+            var expire = now.AddHours(1);
+            Console.WriteLine( user1 + " -> " + expire);
+            
+            _unit.SetPrincipal(user1,now.AddHours(1),claimsPrincipal);
+            
+            Assert.IsNotNull(_unit.GetPrincipal(user1));
+            Assert.IsNull(_unit.GetPrincipal(user1));
+        }
+        
+        [TestMethod]
+        public void GetNonExpiredItemShouldReturn()
+        {
+            var now = DateTimeOffset.UtcNow;
+            _clock.SetupSequence(c => c.UtcNow)
+                .Returns(now).Returns(now)      // SET 
+                .Returns(now.AddMinutes(59))    // GET a&1
+                .Returns(now.AddMinutes(59))    // GET b&1
+                .Returns(now.AddMinutes(61))    // GET a&1
+                .Returns(now.AddMinutes(61));   // GET b&1
+            
+            const string user1 = "a&1";
+            var claimsPrincipal1 = new ClaimsPrincipal();
+            const string user2 = "b&1";
+            var claimsPrincipal2 = new ClaimsPrincipal();
+            
+            _unit.SetPrincipal(user1,now.AddHours(1),claimsPrincipal1);
+            _unit.SetPrincipal(user2,now.AddHours(2),claimsPrincipal2);
+            
+            // 59 minutes later
+            Assert.IsNotNull(_unit.GetPrincipal(user1));
+            Assert.IsNotNull(_unit.GetPrincipal(user2));
+            
+            // 61 minutes later
+            Assert.IsNull(_unit.GetPrincipal(user1));
+            Assert.IsNotNull(_unit.GetPrincipal(user2));
         }
     }
 }
