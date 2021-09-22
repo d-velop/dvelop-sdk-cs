62d56d9b8ca3fdc1e31fffde39225d23caaabcc4
Veit Kunz <git@veit-kunz.de>
2021-09-22 14:48:40 +0200

    Separated logging of scope information and logging of custom attributes

---
+++ dvelop-sdk-logging/Logging.Abstractions/Scope/CustomAttributesLogScope.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace Dvelop.Sdk.Logging.Abstractions.Scope
+{
+    public interface ICustomAttributesLogScope
+    {
+        Dictionary<string, object> Items { get; }
+        string Name { get; }
+    }
+
+    public class CustomAttributesLogScope : ICustomAttributesLogScope
+    {
+        public Dictionary<string, object> Items { get; }
+        public string Name { get; }
+
+        public CustomAttributesLogScope(string name, Dictionary<string, object> items)
+        {
+            Name = name;
+            Items = items;
+        }
+    }
+}
--- dvelop-sdk-logging/Logging.OtelJsonConsole/Scope/ScopeInfo.cs
+++ dvelop-sdk-logging/Logging.OtelJsonConsole/Scope/ScopeInfo.cs
@@ -6,7 +6,8 @@ namespace Dvelop.Sdk.Logging.OtelJsonConsole.Scope
     internal class ScopeInfo
     {
         public bool Visible { get; set; }
-        public IList<object> CustomAttributes { get; set; }
+        public IList<ICustomAttributesLogScope> CustomAttributes { get; set; }
+        public IList<object> Scopes { get; set; }
         public TenantLogScope TenantLogScope { get; set; }
         public TracingLogScope TracingLogScope { get; set; }
     }
