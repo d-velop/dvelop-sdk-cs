8b65fc8acbf35bd857e552a9cca3b4988c46c4e9
Veit Kunz <git@veit-kunz.de>
2021-09-24 14:31:45 +0200

    Added documentation comments (#20)

--- dvelop-sdk-logging/Logging.Abstractions/State/Default/DatabaseLogAttributeState.cs
+++ dvelop-sdk-logging/Logging.Abstractions/State/Default/DatabaseLogAttributeState.cs
@@ -6,9 +6,29 @@ namespace Dvelop.Sdk.Logging.Abstractions.State.Default
 {
     public class DatabaseLogAttributeState : CustomLogAttributeState
     {
+        /// <summary>
+        /// If no <a href="https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/trace/semantic_conventions/database.md#call-level-attributes-for-specific-technologies">tech-specific attribute</a>
+        /// is defined, this attribute is used to report the name of the database being accessed.
+        /// For commands that switch the database, this should be set to the
+        /// target database (even if the command fails).
+        /// <example><c>customers</c>; <c>main</c></example>
+        /// </summary>
         public string Name { get; set; }
+        /// <summary>
+        /// The database statement being executed.
+        /// <example><c>SELECT * FROM wuser_table</c>; <c>SET mykey "WuValue"</c></example>
+        /// </summary>
         public string Statement { get; set; }
+        /// <summary>
+        /// The name of the operation being executed, e.g. the
+        /// <a href="https://docs.mongodb.com/manual/reference/command/#database-operations">MongoDB command name</a>
+        /// such as findAndModify, or the SQL keyword.
+        /// <example><c>findAndModify</c>; <c>HMSET</c>; <c>SELECT</c></example>
+        /// </summary>
         public string Operation { get; set; }
+        /// <summary>
+        /// Measures the duration of the db request in milliseconds.
+        /// </summary>
         public long Duration { get; set; }
 
         public override IEnumerable<CustomLogAttribute> Attributes
--- dvelop-sdk-logging/Logging.Abstractions/State/Default/HttpLogAttributeState.cs
+++ dvelop-sdk-logging/Logging.Abstractions/State/Default/HttpLogAttributeState.cs
@@ -6,16 +6,61 @@ namespace Dvelop.Sdk.Logging.Abstractions.State.Default
 {
     public class HttpLogAttributeState : CustomLogAttributeState
     {
+        /// <summary>
+        /// HTTP request method.
+        /// <example><c>GET</c>; <c>POST</c>; <c>HEAD</c></example>
+        /// </summary>
         public string Method { get; set; }
+        /// <summary>
+        /// <a href="https://tools.ietf.org/html/rfc7231#section-6">HTTP response status code</a>.
+        /// <example><c>200</c></example>
+        /// </summary>
         public int StatusCode { get; set; }
+        /// <summary>
+        /// Full HTTP request URL in the form <c>scheme://host[:port]/path?query[#fragment]</c>.
+        /// Usually the fragment is not transmitted over HTTP, but if it is known, it should be included nevertheless.
+        /// <example><c>https://www.foo.bar/search?q=OpenTelemetry#SemConv</c></example>
+        /// </summary>
         public string Url { get; set; }
+        /// <summary>
+        /// The full request target as passed in a HTTP request line or equivalent.
+        /// <example><c>/path/12314/?q=ddds#123</c></example>
+        /// </summary>
         public string Target { get; set; }
+        /// <summary>
+        /// The value of the <a href="https://tools.ietf.org/html/rfc7230#section-5.4">HTTP host header</a>.
+        /// An empty Host header should also be reported, see note.
+        /// <example><c>www.example.org</c></example>
+        /// </summary>
         public string Host { get; set; }
+        /// <summary>
+        /// The URI scheme identifying the used protocol.
+        /// <example><c>http</c>; <c>https</c></example>
+        /// </summary>
         public string Scheme { get; set; }
+        /// <summary>
+        /// The matched route (path template).
+        /// <example><c>/users/:userId</c></example>
+        /// </summary>
         public string Route { get; set; }
+        /// <summary>
+        /// Value of the <a href="https://tools.ietf.org/html/rfc7231#section-5.5.3">HTTP User-Agent</a> header sent by the client.
+        /// <example><c>CERN-LineMode/2.15 libwww/2.17b3</c></example>
+        /// </summary>
         public string UserAgent { get; set; }
+        /// <summary>
+        /// The IP address of the original client behind all proxies, if known
+        /// (e.g. from <a href="https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Forwarded-For">X-Forwarded-For</a>).
+        /// <example><c>83.164.160.102</c></example>
+        /// </summary>
         public string ClientIp { get; set; }
+        /// <summary>
+        /// Measures the duration of the inbound HTTP request in milliseconds.
+        /// </summary>
         public long ServerDuration { get; set; }
+        /// <summary>
+        /// Measure the duration of the outbound HTTP request in milliseconds.
+        /// </summary>
         public long ClientDuration { get; set; }
 
         public override IEnumerable<CustomLogAttribute> Attributes
