47d0b4be81d2d604e046d3db6ff711e1fad5e171
Burdinski Stefan <stefan.burdinski@d-velop.de>
2021-11-10 14:47:41 +0100

    Added DTOs for Identity Provider session token requesting

---
+++ dvelop-sdk-identityprovider/IdentityProviderDtos/SessionTokenRequestDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Dvelop.Sdk.IdentityProvider.Dto
+{
+    public class SessionTokenRequestDto
+    {
+        public string Appname { get; set; }
+
+        public string Callback { get; set; }
+
+        public string Requestid { get; set; }
+    }
+}
